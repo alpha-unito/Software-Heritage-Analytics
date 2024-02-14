0495028ecadd08be6b501e91f3d5ec38bccf4f35
gspinate <gspinate@c3sfr1.di.unito.it>
2024-02-14 15:31:25 +0100

    Updated utils.py with various improvements and fixes.

--- Orchestrator/utils.py
+++ Orchestrator/utils.py
@@ -6,10 +6,9 @@ from typing import Dict
 from language_extensions import get_extension_by_language_name
 import gzip
 import csv
-import logging
-import logging.handlers
 from datetime import datetime
 import multiprocessing
+
 
 class SingletonType(type):
     # meta class for making a class singleton
@@ -18,27 +17,26 @@ class SingletonType(type):
         try:
             return cls.__instance
         except AttributeError:
-            cls.__instance = super(SingletonType, cls).__call__(*args, **kwargs)
+            cls.__instance = super(
+                SingletonType, cls).__call__(*args, **kwargs)
             return cls.__instance
 
 
-
 class Logger(object):
- 
-     # singleton class
+
+    # singleton class
     __metaclass__ = SingletonType
 
-    def __init__(self,*args, **kwargs):
- 
-        #log_time = datetime.now().strftime("%Y%m%d-%H:%M:%S")
+    def __init__(self, *args, **kwargs):
+
+        # log_time = datetime.now().strftime("%Y%m%d-%H:%M:%S")
         log_time = ""
         self.write_to_file = True
         app_name = args[0]
         if self.write_to_file is True:
             self.file_name = f"logs/APP-{app_name}-{log_time}.log"
-            
 
-    def debug(self,header,msg=""):
+    def debug(self, header, msg=""):
         time = datetime.now().strftime("%H:%M:%S.%f")
         log_msg = f"[{time} {multiprocessing.current_process().name}] {header} {msg}\n"
         if self.write_to_file is True:
@@ -48,95 +46,98 @@ class Logger(object):
             print(log_msg)
 
 
+def unpackProjectFile(project_id, tar_file):
+    ret = {}
 
-def unpackProjectFile(project_id,tar_file):
-        ret = {} 
-        with tarfile.open(fileobj=tar_file,mode="r:gz" ) as file_list:
-            for f in file_list:
-                # print(f.name)
-                try:
-                    ret[f.name] = {'project_id':project_id,
-                                      'file_basename': os.path.basename(f.name),
-                                      'file_name': f.name, 
-                                      'file_type': "",
-                                      'data': base64.b64encode(file_list.extractfile(f.name).read()).decode()}
-                except:
-                    pass
-                
-        return ret
+    vista_di_memoria = tar_file.getbuffer()
+    dimensione_buffer = len(vista_di_memoria)
 
-def unpackProjectFileData(project_id,tar_file_data):
-        ret = {}
-        file_data = gzip.decompress(tar_file_data)
-        tarfile.TarInfo.frombuf(tar_file_data )
-        with tarfile.open(project_entry,"r:gz" ) as file_list:
+    if dimensione_buffer <= (500*1024*1024):
+        with tarfile.open(fileobj=tar_file, mode="r:gz") as file_list:
             for f in file_list:
-                #print(file.name)
                 try:
-                    ret[f.name] = {'project_id':project_id,
-                                      'file_basename': os.path.basename(f.name),
-                                      'file_name': f.name,
-                                      'file_type': "",
-                                      'data': base64.b64encode(file_list.extractfile(f.name).read()).decode()}
+                    ret[f.name] = {'project_id': project_id,
+                                   'file_basename': os.path.basename(f.name),
+                                   'file_name': f.name,
+                                   'file_type': "",
+                                   'data': base64.b64encode(file_list.extractfile(f.name).read()).decode()}
                 except:
                     pass
 
-        return ret
+    return ret
 
 
-def scheduleFileOnStream(language,queue_list,file_list: Dict):
+def unpackProjectFileData(project_id, tar_file_data):
+    ret = {}
+    file_data = gzip.decompress(tar_file_data)
+    tarfile.TarInfo.frombuf(tar_file_data)
+    with tarfile.open(project_entry, "r:gz") as file_list:
+        for f in file_list:
+            # print(file.name)
+            try:
+                ret[f.name] = {'project_id': project_id,
+                               'file_basename': os.path.basename(f.name),
+                               'file_name': f.name,
+                               'file_type': "",
+                               'data': base64.b64encode(file_list.extractfile(f.name).read()).decode()}
+            except:
+                pass
+
+    return ret
+
+
+def scheduleFileOnStream(language, queue_list, file_list: Dict):
     q_idx = 0
     ret = ""
-    ret += "-----------------------------------\n"    
-    #print("[+] Scheduling file for send (language:" + language + ")")
+    ret += "-----------------------------------\n"
+    # print("[+] Scheduling file for send (language:" + language + ")")
     extensions = ""
     num_file = 0
     data_size = 0
     # print(file_list)
-    ret += "[+] list "  + str(len(file_list)) + " files\n" 
-    #print("[+] list "  + str(len(file_list)) + " files")
+
+    ret += "[+] list " + str(len(file_list)) + " files\n"
+    # print("[+] list "  + str(len(file_list)) + " files")
     if language != "":
         extensions = get_extension_by_language_name(language)
+    if not isinstance(file_list, dict):
+        file_list = {}
     for file, file_data in file_list.items():
         filename, file_extension = os.path.splitext(file)
-        #print("[+] stream:"+ str(q_idx) + " " + file)
+        # print("[+] stream:"+ str(q_idx) + " " + file)
         if extensions != "":
             try:
                 extensions.index(file_extension)
             except:
-                #print("[+] PRUNED " + file)
+                # print("[+] PRUNED " + file)
                 continue
         num_file = num_file + 1
-        data_size += len(file_data) 
-        #print("[+] stream:"+ str(q_idx) + " " + file)
-        #print("[+] stream:"+ str(q_idx) + " " + str(file_data))
+        data_size += len(file_data)
         queue_list[q_idx].put(file_data.copy())
-        #print(queue_list[q_idx])
-        #print(queue_list[q_idx].qsize())
-        
         q_idx = (q_idx + 1) % len(queue_list)
-    #print("[+] stream "  + str(num_file) + " files")    
-    ret += "[+] stream "  + str(num_file) + " files, tot byte"+str(data_size)+"\n"+"-----------------------------------\n"
-    #print("-----------------------------------")
+    ret += "[+] stream " + str(num_file) + " files, tot byte" + \
+        str(data_size)+"\n"+"-----------------------------------\n"
+    # print("-----------------------------------")
     return ret
 
-def logInfo(call_time,op, project_id, outcome, execution_time, error):
+
+def logInfo(call_time, op, project_id, outcome, execution_time, error):
     log_info = dict()
     log_info["self.project_id"] = project_id
     log_info["call_time"] = call_time
     log_info["op"] = op
     log_info["outcome"] = outcome
     log_info["error"] = error
-    log_info["execution_time"] = execution_time - log_info["call_time"] 
+    log_info["execution_time"] = execution_time - log_info["call_time"]
     return log_info
 
-def logCSV(appname,log_string_dict):
-    #return
-    
-    with open(f'logs/{appname}.csv', mode='a+') as csv_file:
-        fieldnames = ['call_time', 'op', 'project_id', 'outcome', 'execution_time', 'error' ]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-     
-        writer.writerow(log_string_dict)
-    
 
+def logCSV(appname, log_string_dict):
+    # return
+
+    with open(f'logs/{appname}.csv', mode='a+') as csv_file:
+        fieldnames = ['call_time', 'op', 'project_id',
+                      'outcome', 'execution_time', 'error']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writerow(log_string_dict)
