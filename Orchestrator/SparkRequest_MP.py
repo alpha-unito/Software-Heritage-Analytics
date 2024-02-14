eb9c16ab95ef18b72f9ef66b3098089bff26a474
gspinate <gspinate@c3sfr1.di.unito.it>
2024-02-14 15:30:27 +0100

    Updated SparkRequest_MP.py for improved file handling and performance optimization.

--- Orchestrator/SparkRequest_MP.py
+++ Orchestrator/SparkRequest_MP.py
@@ -1,17 +1,13 @@
 import json
-import logging
-import os, sys
-from pathlib import Path
-from multiprocessing import Process, Queue
-import socket 
-#from threading import Thread 
+import sys
+import socket
 import time
 from config import _CONFIG
 import uuid
 
-#logging.basicConfig(level=logging.DEBUG,format='[%(levelname)s] (%(threadName)-9s) %(message)s',)
+# logging.basicConfig(level=logging.DEBUG,format='[%(levelname)s] (%(threadName)-9s) %(message)s',)
 ext_to_exclude = [
-    ".jpg", ".jpeg", ".png", ".gif", ".tiff", 
+    ".jpg", ".jpeg", ".png", ".gif", ".tiff",
     ".mp3", ".wav", ".flac",
     ".mp4", ".mov", ".avi", ".mkv", ".webm", ".wmv", ".flv", ".mpeg", ".ogg", ".dvx", ".rm", ".asf", ".3pg"
     ".doc", ".docx", ".pdf",
@@ -23,87 +19,64 @@ ext_to_exclude = [
     ".xml",
     ".csv"
 ]
-class SparkRequest(): 
- 
-    def __init__(self,conn,ip,port,q): 
-        #Thread.__init__(self) 
-        self.ip = ip 
-        self.port = port 
+
+
+class SparkRequest():
+
+    def __init__(self, conn, ip, port, q):
+        # Thread.__init__(self)
+        self.ip = ip
+        self.port = port
         self.conn = conn
         self.q = q
         self.conta = 0
         self.id = uuid.uuid4()
-        #print(self.q)
-        #print(self.q.qsize())
-        print("[**] New server socket thread started for spark connetion from " + ip + ":" + str(port)) 
- 
-    def run(self): 
-        #for i in range (10) : 
-        #    MESSAGE = "File DATA " + str(i) + "\n"
-        #    self.conn.sendall(MESSAGE.encode())  # echo 
-        
-        while True:
-            #print("[**] Get from queue for "+ self.ip + ":" + str(self.port))
-            #print(self.q)
-            #print("[**] queue size " + self.q.qsize())
-            # data_file = self.q.get()
+        # print(self.q)
+        # print(self.q.qsize())
+        print("[**] New server socket thread started for spark connetion from " +
+              ip + ":" + str(port))
 
+    def run(self):
+
+        while True:
             data_file = None
             if not self.q.empty():
                 data_file = self.q.get_nowait()
-                if(data_file == "END"):
-                    print("END <------------------------------")
-                           
+                if (data_file == "END"):
+                    print("[**] Spark request - END")
             try:
-                #print("[**] " + data_file)
+                # print("[**] " + data_file)
                 if data_file == "END":
-                    end_msg = {'project_id':'EOS','file_name': '','file_type': '','data':''}
+                    end_msg = {'project_id': 'EOS', 'file_name': '',
+                               'file_type': '', 'data': ''}
                     self.conn.send(json.dumps(end_msg).encode() + b'\n')
-                    #self.conn.send(json.dumps('{"project_id":"EOS","file_name":"","file_type": "","data":""}').encode() + b'\n')
                     print("[**] Data is END... EOS")
-                    #self.q.task_done()
-                    #return
                     self.conn.close()
                     break
                 if data_file is None:
-                    # end_msg = {'project_id':'WAIT','file_name': '','file_type': '','data':''}
-                    # self.conn.send(json.dumps(end_msg).encode() + b'\n')
-                    # #self.conn.send(json.dumps('{"project_id":"EOS","file_name":"","file_type": "","data":""}').encode() + b'\n')
-                    print("[**] Data is None... WAIT") 
+                    self.conta += 1
+                    if (self.conta == 10000):
+                        time.sleep(5)
+                        self.conta = 0
                     time.sleep(_CONFIG["send_sleep_wait"])
-                    continue                
+                    continue
                 if any((data_file["file_basename"]).endswith(ext) for ext in ext_to_exclude):
-                    print(f"[ESCLUSO]:", data_file["file_basename"])
-                    continue 
-                print(f"[OK]:", data_file["file_basename"])
+                    continue
                 time.sleep(_CONFIG["send_sleep_debug"])
                 msg = json.dumps(data_file)
-                #print("msg: " + data_file["file_name"]);
+
                 self.conn.send(msg.encode() + b'\n')
-                self.conta += 1
-                # project_id = data_file['project_id']
-                # nome = data_file['file_basename']
-                # print(f"[{self.id}] - {nome} ====> CONTA: {self.conta}")
+
                 # print(f"{project_id} - {name}")
             except:
                 print("[**] Unexpected socket error - Spark worker has close socket")
                 self.conn.close()
-                # return
                 sys.exit()
-        
-            #print("[**] Project:" + data_file['project_id'] +  "File:"+ data_file['file_name'] + " sended to "+ self.ip + ":" + str(self.port) )
-            #logging.debug("[**] File "+ data_file['file_name'] +" sended to ")
-            #self.q.task_done()        
-            #if data_file == "EOS":
-            #    break
 
         while True:
             try:
-                #end_msg = {'project_id':'EOS','file_name': '','file_type': '','data':''}
-                #self.conn.send(json.dumps(end_msg).encode() + b'\n')
-                #self.conn.send("Test connection")
-                   
-                result = self.conn.getsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE)
+                result = self.conn.getsockopt(
+                    socket.SOL_SOCKET, socket.SO_KEEPALIVE)
                 if result == 0:
                     print('Il socket è stato chiuso dal client')
                     time.sleep(10)
@@ -111,8 +84,7 @@ class SparkRequest():
                     break
                 else:
                     print('Il socket è ancora aperto')
-                #print("[**] Data is None... EOS")
-                    
+
             except:
                 print("[**] Spark worker has close socket")
                 self.conn.close()
