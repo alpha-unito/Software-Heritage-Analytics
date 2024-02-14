90592091b003c7b12094499fb852e8a587e69b4b
gspinate <gspinate@c3sfr1.di.unito.it>
2024-02-14 15:29:51 +0100

    Refactored PyCachemire.py for improved client handling and better server scalability.

--- Orchestrator/pycachemire.py
+++ Orchestrator/pycachemire.py
@@ -1,39 +1,25 @@
-import json
-import logging
-import os
 import sys
-import socket 
-from pathlib import Path
+import socket
 from multiprocessing import Process
-from threading import Thread 
-#from CtrlRequestThread import CtrlRequestThread
+from threading import Thread
 from CtrlRequestThread_MP import CtrlRequestThread
-from types import SimpleNamespace
-#from Cache import cache
+
 from config import _CONFIG
-import signal 
+import signal
 
 # CONFIG
-CTRL_TCP_IP = _CONFIG["bind_address"] 
-CTRL_TCP_PORT = _CONFIG["port"] 
+CTRL_TCP_IP = _CONFIG["bind_address"]
+CTRL_TCP_PORT = _CONFIG["port"]
 CTRL_BUFFER_SIZE = 1024
-MAX_CLIENT =  _CONFIG["num_max_app_client"]
-#cache.num_element = 10
+MAX_CLIENT = _CONFIG["num_max_app_client"]
+# cache.num_element = 10
 
-tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-tcpServer.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-tcpServer.bind((CTRL_TCP_IP, CTRL_TCP_PORT)) 
-ctrlThreads = [] 
+tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcpServer.bind((CTRL_TCP_IP, CTRL_TCP_PORT))
+ctrlThreads = []
 
-#print(cache) 
 
-#def handler(signum, frame):
-#    res = input("Ctrl-c was pressed. Do you really want to exit? y/n ")
-#    if res == 'y':
-#        exit(1)
- 
-#signal.signal(signal.SIGINT, handler)
 
 def exit_gracefully(signum, frame):
     # restore the original signal handler as otherwise evil things will happen
@@ -48,8 +34,9 @@ def exit_gracefully(signum, frame):
         print("Ok ok, quitting")
         sys.exit(1)
 
-    # restore the exit gracefully handler here    
+    # restore the exit gracefully handler here
     signal.signal(signal.SIGINT, exit_gracefully)
+
 
 original_sigint = signal.getsignal(signal.SIGINT)
 signal.signal(signal.SIGINT, exit_gracefully)
@@ -57,21 +44,23 @@ signal.signal(signal.SIGINT, exit_gracefully)
 
 num_client = 0
 ctrlClient = []
-while True: 
+while True:
     try:
-        tcpServer.listen(4) 
-        print("PyCachemire server : Waiting for connections from Dashboard clients on port "+ str(CTRL_TCP_PORT) +" ...") 
-        (conn, (ip,port)) = tcpServer.accept() 
-        ctrl = CtrlRequestThread(conn,ip,port,num_client) 
+        tcpServer.listen(4)
+        print("PyCachemire server : Waiting for connections from Dashboard clients on port " +
+              str(CTRL_TCP_PORT) + " ...")
+        (conn, (ip, port)) = tcpServer.accept()
+        ctrl = CtrlRequestThread(conn, ip, port, num_client)
         newclient = Process(target=ctrl.run)
-        newclient.start() 
-        # ctrlClient.append(newclient) 
-        num_client = num_client + 1    
-    except Exception as e: print(e)
+        newclient.start()
+        # ctrlClient.append(newclient)
+        num_client = num_client + 1
+    except Exception as e:
+        print(e)
 
 
 print("PyCachemire wait client end..")
-for t in ctrlClient: 
+for t in ctrlClient:
     t.join()
 
 print("PyCachemire exit")
